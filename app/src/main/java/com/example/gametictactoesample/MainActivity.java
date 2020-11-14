f6db65a1b1ee9018dd7abeaf3433238c97c1f7fe
varun-rajareddy <66180708+varun-rajareddy@users.noreply.github.com>
2020-11-13 20:48:37 -0500

    first commit and testing

---
+++ app/src/main/java/com/example/gametictactoesample/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.gametictactoesample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public static final String TAG = "MainActivity";
+    public static final String KEY_NAME = "name";
+
+    public EditText editTextName;
+
+    public FirebaseFirestore db = FirebaseFirestore.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextName = findViewById(R.id.edit_Text_Name);
+
+
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
+    }
+
+
+    public void openActivity2() {
+
+        Intent intent = new Intent(this, Activity2.class);
+
+        startActivity(intent);
+    }
+
+
+
+    public void saveNote(View v) {
+        String name = editTextName.getText().toString();
+
+        Map<String, Object> note = new HashMap<>();
+        note.put(KEY_NAME , name);
+
+        db.collection("Game").document("Tic tac toe notes").set(note)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Name saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+    }
+}
